40b1fe2d0b40dd137b0748d9a7998904429d112e
Andrew Bautista <andrewbautista85@gmail.com>
2026-06-23 15:42:09 -0400

    Implement login initialization script
    Add initialization script to login process

---
+++ triggers/login.js
@@ -0,0 +1,7 @@
+/* Name: Util: Initlialize and equip on login
+Type: plain
+Pattern: You finally recover your */
+
+// Execute the following javascript:
+  gwc.connection.send("initialize", true)
+  // ... rest of whatever aliases you want to run on login
