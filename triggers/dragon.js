5152943a68eedc0426a69afc2e23dc7a3e563d95
Andrew Bautista <andrewbautista85@gmail.com>
2026-06-23 16:12:04 -0400

    Refactor dragon trigger comments for consistency
    Updated comments to improve formatting and clarity.

--- triggers/dragon.js
+++ triggers/dragon.js
@@ -1,36 +1,44 @@
 // A collection of dragon triggers. 
 // Make note of which triggers use javascript and which use direct genesis commands.
 
-/* Name: Util: Dragon - is Present
+/* 
+Name: Util: Dragon - is Present
 Type: regexp
-Pattern: circling over the battlefield with cruel interest */
+Pattern: circling over the battlefield with cruel interest 
+*/
 
 // Execute the following javascript:
   gwc.userdata.dragonPresent = true;
 
 // ----------------------------------------------------
 
-/* Name: Util: Dragon - is Gone
+/* 
+Name: Util: Dragon - is Gone
 Type: regexp
-Pattern: soars up into the sky and disappears  */
+Pattern: soars up into the sky and disappears  
+*/
 
 // Execute the following javascript:
    gwc.userdata.dragonPresent = false;
 
 // ----------------------------------------------------
 
-/* Name: Util: Dragon - Dfear
+/* 
+Name: Util: Dragon - Dfear
 Type: regexp
-Pattern: You feel you are once again able to summon dragons to your aid|soars up into the sky and disappears */
+Pattern: You feel you are once again able to summon dragons to your aid|soars up into the sky and disappears 
+*/
 
 // Execute the following commands:
    dfear
 
 // ----------------------------------------------------
 
-/* Name: Util: Dragon - Attack
+/* 
+Name: Util: Dragon - Attack
 Type: regexp
-Pattern: hovering high above you, ready to make another attack */
+Pattern: hovering high above you, ready to make another attack 
+*/
 
 // Execute the following javascript:
   gwc.connection.send(`${gwc.userdata.dragonAttack || 'dswoop'}`)
